074a3653f9a2991cfdeed21b61e3afdec92edb7f
yan <934148981@qq.com>
2020-09-23 18:24:16 +0800

    spring 工厂 容器 原理拆解

---
+++ springIOC/src/main/java/com/github/dao/IAccountDao.java
@@ -0,0 +1,14 @@
+package com.github.dao;
+
+/**
+* 账户的持久层接口
+* @author 黑马程序员
+* @Company http://www.ithiema.com
+* @Version 1.0
+*/
+public interface IAccountDao {
+    /**
+     * 保存账户
+     */
+    void saveAccount();
+}
